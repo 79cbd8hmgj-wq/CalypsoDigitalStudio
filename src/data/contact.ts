2a2cc0533e39d2de7d3e2e5de800178306dd2014
79cbd8hmgj-wq <79cbd8hmgj@privaterelay.appleid.com>
2026-07-29 15:51:29 -0400

    Add studio contact information

---
+++ src/data/contact.ts
@@ -0,0 +1,12 @@
+export const contact = {
+  email: 'calydigital@outlook.com',
+  emailHref: 'mailto:calydigital@outlook.com',
+  instagram: {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/calypsodigitalstudio/'
+  },
+  facebook: {
+    label: 'Facebook',
+    href: 'https://www.facebook.com/evan.lebrecht.2025/'
+  }
+} as const;
